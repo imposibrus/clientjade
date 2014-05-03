68531ea0e03c15ee912602cd2199342fb007e08b
imposibrus <imposibrus@gmail.com>
2014-05-04 00:03:56 +0400

    feature - add grunt task

---
+++ tasks/clientjade.js
@@ -0,0 +1,13 @@
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('clientjade', 'clientjade desc', function() {
+    var compile = require('../lib/compile'),
+        done = this.async(),
+        ths = this;
+
+    compile({files: ths.data.src, compress: false}, function(err, out) {
+      grunt.file.write(ths.data.dest, out);
+      done();
+    });
+  });
+};
